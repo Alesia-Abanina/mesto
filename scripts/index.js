67c57e3b256523977dca8e97862914c3eee33fe4
Alesia Abanina <alesia.abanina@gmail.com>
2020-12-22 20:32:34 +1100

    final changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
-const togglePopup = (event) => {
-  event.target.classList.toggle('form_display_active');
+let togglePopup = () => {
+  formOverlay.classList.toggle('form_display_active');
 }
 
-const formOverlay = document.querySelector('.form');
+let formOverlay = document.querySelector('.form');
 formOverlay.addEventListener('click', (event) => {
   if (event.target === event.currentTarget) {
     togglePopup();
   }
 });
 
-const profileEditBtn = document.querySelector('.profile__edit');
-profileEditBtn.addEventListener('click', togglePopup);
+let nameInput = formOverlay.querySelector('#name');
+let descriptionInput = formOverlay.querySelector('#description');
+let nameTitle = document.querySelector('.profile__title');
+let descriptionSubtitle = document.querySelector('.profile__subtitle');
 
-const closeBtn = formOverlay.querySelector('.form__close-btn');
+let profileEditBtn = document.querySelector('.profile__edit');
+profileEditBtn.addEventListener('click', () => {
+  nameInput.value = nameTitle.textContent;
+  descriptionInput.value = descriptionSubtitle.textContent;
+  togglePopup();
+});
+
+let closeBtn = formOverlay.querySelector('.form__close-btn');
 closeBtn.addEventListener('click', togglePopup);
 
-const nameInput = formOverlay.querySelector('#name');
-const descriptionInput = formOverlay.querySelector('#description');
-const nameTitle = document.querySelector('.profile__title');
-const descriptionSubtitle = document.querySelector('.profile__subtitle');
-nameInput.value = nameTitle.textContent;
-descriptionInput.value = descriptionSubtitle.textContent;
-
-const formPopup = document.querySelector('.form__popup');
+let formPopup = document.querySelector('.form__popup');
 formPopup.addEventListener('submit', (event) => {
   event.preventDefault();
   nameTitle.textContent = nameInput.value;
@@ -30,8 +32,8 @@ formPopup.addEventListener('submit', (event) => {
   togglePopup();
 });
 
-const likeButtons = document.querySelectorAll('.element__like');
-for (let i = 0; i < likeButtons.length; i++){
+let likeButtons = document.querySelectorAll('.element__like');
+for (let i = 0; i < likeButtons.length; i++) {
   likeButtons[i].addEventListener('click', (event) => {
     event.target.classList.toggle('element__like_type_active');
   });
